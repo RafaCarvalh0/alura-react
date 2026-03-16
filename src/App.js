05b14d53dc039a1ab05376cc30c4b1e259b66e88
RafaCarvalh0 <rafacarvalho601@gmail.com>
2026-03-16 01:22:37 -0300

    style melhorado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
 import Logo from './components/Logo';
+import perfil from './image/perfil.svg';
+import sacola from './image/sacola.svg';
+
+const textoOpcoes = ['CATEGORIAS', 'MINHA ESTANTE', 'FAVORITOS'];
+const icones = [perfil, sacola];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        
         <Logo />
+
+        <ul className="opcoes">
+          {textoOpcoes.map((texto) => (<li className="opcao"> <p>{texto}</p> </li> ))}
+        </ul>
+
+        <ul className="icones">
+          {icones.map((icone) => (<li className="icone"> <img src={icone} alt="Ícone" /> </li> ))}
+        </ul>
+
       </header>
     </div>
   );
