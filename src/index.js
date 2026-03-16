59e8abc227197a4ca44f1fabeb3fd31fb01873a8
RafaCarvalh0 <rafacarvalho601@gmail.com>
2026-03-16 16:07:23 -0300

    feature/primeira-rota

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import  { createGlobalStyle } from 'styled-components';
-import { BrowserRouter } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -29,7 +29,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GlobalStyle />
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/favoritos" element={<p>favoritos</p>} />
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
